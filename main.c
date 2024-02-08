cec21c8c233f8836aea887a28bed9b4ce020b5e2
Thomas Pennant <pennantthomas@yahoo.com>
2024-02-08 01:40:31 -0500

    ammended the main function in main.c, corrected all linker errors

--- main.c
+++ main.c
@@ -7,18 +7,25 @@
 
 int main()
 {
-	printf("Enter the name of the image: \n");
-	scanf_s("%50s", &img, 50);
-	printf("Enter the image array in the form of an array of depth 2, e.g. {{1, 0, 0}}");
-	int input_image;
-	scanf_s("%d", &input_image);
-	int* ptr_image = &image;
-	*ptr_image = input_image;
-
+	// IMPORTANT: update the image array with the image data in the vars.h file, this should include the full array and the width and height of the image
 	int height_s = sizeof(image);
 	int width_s = sizeof(image[0]);
-
-	fprintf(stdout, "select an image type to convert by entering one of the following numbers: 1 = pbm, 2 = pgm, 3 = ppm");
+	printf("IMPORTANT: update the image array with the image data in the vars.h file\nthis should include the full array and the width and height of the image\n\n");
+	printf("Enter the name of the image: \n");
+	scanf_s("%50s", &img, 50);
+	if (img == NULL)
+	{
+		fprintf(stderr, "Invalid input");
+	}
+	else if (sizeof(img) > 50)
+	{
+		fprintf(stderr, "The image name is too long");
+	}
+	else
+	{
+		fprintf(stdout, "The image name is now: %s\n\n", img);
+	}
+	fprintf(stdout, "select an image type to convert by entering one of the following numbers: 1 = pbm, 2 = pgm, 3 = ppm\n");
 	int temp;
 	scanf_s("%d", &temp);
 		if (temp == 1)
