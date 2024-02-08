61a85bbb91ed219d9c0c99783d2ac0916d0fcc68
Thomas Pennant <pennantthomas@yahoo.com>
2024-02-07 19:24:36 -0500

    Add project files.

---
+++ image.h
@@ -0,0 +1,91 @@
+#pragma once
+#include <stdio.h>
+#include "vars.h"
+#ifdef _MSC_VER
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+int image_pgm[HEIGHT][WIDTH];
+int image_ppm[HEIGHT][WIDTH];
+int image_pbm[HEIGHT][WIDTH];
+
+naming()
+{
+	printf("Enter the name of the image: \n");
+	scanf("%s", &img);
+}
+
+
+void pgm()
+{
+    // add a script for calculating width and height of array
+    int i, j, temp = 0;
+
+    FILE* pgm_img;
+    pgm_img = fopen(strcat(img, ".pgm"), "wb");
+
+    fprintf(pgm_img, "P2\n");
+
+    fprintf(pgm_img, "%d %d\n", WIDTH, HEIGHT);
+
+    fprintf(pgm_img, "255\n");
+    int cnt = 0;
+    for (i = 0; i < HEIGHT; i++) {
+        for (j = 0; j < WIDTH; j++) {
+            temp = image_pgm[i][j];
+            fprintf(pgm_img, "%d ", temp);
+        }
+        fprintf(pgm_img, "\n");
+    }
+    fclose(pgm_img);
+}
+
+
+void pbm()
+{
+    // add a script for calculating width and height of array
+    int i, j, temp = 0;
+
+    FILE* pbm_img;
+    pbm_img = fopen(strcat(img, ".pbm"), "wb");
+
+    fprintf(pbm_img, "P2\n");
+
+    fprintf(pbm_img, "%d %d\n", WIDTH, HEIGHT);
+
+    fprintf(pbm_img, "255\n");
+    int cnt = 0;
+    for (i = 0; i < HEIGHT; i++) {
+        for (j = 0; j < WIDTH; j++) {
+            temp = image_pbm[i][j];
+            fprintf(pbm_img, "%d ", temp);
+        }
+        fprintf(pbm_img, "\n");
+    }
+    fclose(pbm_img);
+}
+
+
+void ppm()
+{
+    // add a script for calculating width and height of array
+    int i, j, temp = 0;
+
+    FILE* ppm_img;
+    ppm_img = fopen(strcat(img, ".ppm"), "wb");
+
+    fprintf(ppm_img, "P2\n");
+
+    fprintf(ppm_img, "%d %d\n", WIDTH, HEIGHT);
+
+    fprintf(ppm_img, "255\n");
+    int cnt = 0;
+    for (i = 0; i < HEIGHT; i++) {
+        for (j = 0; j < WIDTH; j++) {
+            temp = image_ppm[i][j];
+            fprintf(ppm_img, "%d ", temp);
+        }
+        fprintf(ppm_img, "\n");
+    }
+    fclose(ppm_img);
+}
---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "image.h"
+#include "vars.h"
+
+int main()
+{
+	naming();
+	fprintf(stdout, "select an image type to convert");
+	return 0;
+}
