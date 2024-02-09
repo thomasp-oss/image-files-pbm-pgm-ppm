ed67391fe223a49224b86507e632c3c0d77a81e8
Thomas Pennant <pennantthomas@yahoo.com>
2024-02-09 00:36:14 -0500

    new commit 2

--- main.c
+++ main.c
@@ -4,13 +4,13 @@
 #include <stdio.h>
 #include "image.h"
 #include "vars.h"
-// Before running main.c, make sure that the array in the image.h file is updated with the image data and the width and height of the image
+// Before running main.c, make sure that the array in the image.h file is updated
+// with the image data and the width and height of the image
 
 int main()
 {
-	int height_s = sizeof(image);
-	int width_s = sizeof(image[0]);
-	printf("IMPORTANT: update the image array with the image data in the vars.h file\nthis should include the full array and the width and height of the image\n\n");
+	printf("IMPORTANT: update the image array with the image data in the vars.h file\\
+	\nthis should include the full array and the width and height of the image\n\n");
 	printf("Enter the name of the image: \n");
 
 	scanf_s("%50s", &img, 50);
@@ -27,7 +27,8 @@ int main()
 		fprintf(stdout, "The image name is now: %s\n\n", img);
 	}
 
-	fprintf(stdout, "select an image type to convert by entering one of the following numbers: 1 = pbm, 2 = pgm, 3 = ppm\n");
+	fprintf(stdout, "select an image type to convert by entering one of the following\\
+	 numbers: 1 = pbm, 2 = pgm, 3 = ppm\n");
 	int tmp;
 	scanf_s("%d", &tmp);
 		if (tmp == 1)
